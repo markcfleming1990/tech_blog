156cb0805cd4c9a4f613be967afacfac7d594fcd
Mark <markcfleming1990@gmail.com>
2022-04-15 09:51:07 -0400

    got sign up functional

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -2,11 +2,11 @@ async function signupFormHandler(event) {
   event.preventDefault();
 
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector('.username-signup').value.trim();
+  const password = document.querySelector('.password-signup').value.trim();
 
   if (username && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/user', {
           method: 'POST',
           body: JSON.stringify({
               username,
@@ -26,4 +26,4 @@ async function signupFormHandler(event) {
   }
 }
 
-document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);

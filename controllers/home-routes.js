9d97108bb5dd31f18b720f7d050b49a118ae193d
Mark <markcfleming1990@gmail.com>
2022-04-17 11:53:11 -0400

    still running into Eventlistner error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User,Post,Comment} = require('../models');
+const { Post, User, Comment } = require('../models');
+const router = require('express').Router();
 
-
+// get all posts for homepage
 router.get('/', (req, res) => {
     Post.findAll({
             attributes: [
@@ -26,14 +26,8 @@ router.get('/', (req, res) => {
             ]
         })
         .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({
-                plain: true
-            }));
-
-            res.render('homepage', {
-                posts,
-                loggedIn: req.session.loggedIn
-            });
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
             console.log(err);
@@ -41,6 +35,19 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// get single post
 router.get('/post/:id', (req, res) => {
     Post.findOne({
             where: {
@@ -48,8 +55,8 @@ router.get('/post/:id', (req, res) => {
             },
             attributes: [
                 'id',
-                'title',
                 'content',
+                'title',
                 'created_at'
             ],
             include: [{
@@ -68,20 +75,14 @@ router.get('/post/:id', (req, res) => {
         })
         .then(dbPostData => {
             if (!dbPostData) {
-                res.status(404).json({
-                    message: 'No post found with this id'
-                });
+                res.status(404).json({ message: 'No post found with this id' });
                 return;
             }
+            const post = dbPostData.get({ plain: true });
+            console.log(post);
+            res.render('single-post', { post, loggedIn: req.session.loggedIn });
 
-            const post = dbPostData.get({
-                plain: true
-            });
 
-            res.render('single-post', {
-                post,
-                loggedIn: req.session.loggedIn
-            });
         })
         .catch(err => {
             console.log(err);
@@ -89,29 +90,44 @@ router.get('/post/:id', (req, res) => {
         });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
+router.get('/posts-comments', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
 
-    res.render('login');
+            res.render('posts-comments', { post, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
-
-router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-
-    res.render('signup');
-});
-
-
-router.get('*', (req, res) => {
-    res.status(404).send("Can't go there!");
-    // res.redirect('/');
-})
-
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js');
 const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard.js');
+
 
 
 router.use('/', homeRoutes);
